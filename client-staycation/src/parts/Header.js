acc3b857c64b59254cd70821a80c0a485f61ed12
orlandomamudi <orlando.c.mamudi@gmail.com>
2022-11-15 18:27:56 +0800

    Adding packages react-reveal for fade transition

--- client-staycation/src/parts/Header.js
+++ client-staycation/src/parts/Header.js
@@ -1,47 +1,50 @@
 import React from 'react'
+import { Fade } from 'react-reveal';
 
 import Button from '../elements/Button'
 import BrandIcon from './IconText'
 
 export default function Header(props) {
 
-    console.log("tes", props.location);
+  console.log("tes", props.location);
 
-    const getNavLinkClass = (path) => {
-        return props.location.pathname === path ? "active" : ""
-    }
+  const getNavLinkClass = (path) => {
+    return props.location.pathname === path ? "active" : ""
+  }
 
-    return (
-        <header className="spacing-sm">
-            <div className="container">
-                <nav className="navbar navbar-expand-lg navbar-light">
-                    <BrandIcon />
-                    <div className="collapse navbar-collapse">
-                        <ul className="navbar-nav ml-auto">
-                            <li className={`nav-item ${getNavLinkClass("/")}`}>
-                                <Button className="nav-link" type="link" href="/">
-                                    Home
-                                </Button>
-                            </li>
-                            <li className={`nav-item ${getNavLinkClass("/browse-by")}`}>
-                                <Button className="nav-link" type="link" href="/browse-by">
-                                    Browse By
-                                </Button>
-                            </li>
-                            <li className={`nav-item ${getNavLinkClass("/stories")}`}>
-                                <Button className="nav-link" type="link" href="/stories">
-                                    Stories
-                                </Button>
-                            </li>
-                            <li className={`nav-item ${getNavLinkClass("/agents")}`}>
-                                <Button className="nav-link" type="link" href="/agents">
-                                    Agents
-                                </Button>
-                            </li>
-                        </ul>
-                    </div>
-                </nav>
+  return (
+    <Fade>
+      <header className="spacing-sm">
+        <div className="container">
+          <nav className="navbar navbar-expand-lg navbar-light">
+            <BrandIcon />
+            <div className="collapse navbar-collapse">
+              <ul className="navbar-nav ml-auto">
+                <li className={`nav-item ${getNavLinkClass("/")}`}>
+                  <Button className="nav-link" type="link" href="/">
+                    Home
+                  </Button>
+                </li>
+                <li className={`nav-item ${getNavLinkClass("/browse-by")}`}>
+                  <Button className="nav-link" type="link" href="/browse-by">
+                    Browse By
+                  </Button>
+                </li>
+                <li className={`nav-item ${getNavLinkClass("/stories")}`}>
+                  <Button className="nav-link" type="link" href="/stories">
+                    Stories
+                  </Button>
+                </li>
+                <li className={`nav-item ${getNavLinkClass("/agents")}`}>
+                  <Button className="nav-link" type="link" href="/agents">
+                    Agents
+                  </Button>
+                </li>
+              </ul>
             </div>
-        </header>
-    )
+          </nav>
+        </div>
+      </header>
+    </Fade>
+  )
 }
